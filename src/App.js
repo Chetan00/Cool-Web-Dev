95d9ceeb91c90f9ff1ad00eacb351cc8e22fa6c4
Chetan Agrawal <30387373+Chetan00@users.noreply.github.com>
2022-11-13 17:55:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{'Hey There 😊'}</p>
+        <p>{'Your one stop to Learn Web Development is still under construction.'}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
